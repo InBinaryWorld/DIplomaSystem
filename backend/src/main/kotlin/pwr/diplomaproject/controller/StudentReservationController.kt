8b72fb3ced286df5b6428982a51901e7d423f88b
Jacek Wernikowski <jacekwernikow@gmail.com>
2022-02-02 12:04:50 +0100

    Fix startup error

--- backend/src/main/kotlin/pwr/diplomaproject/controller/StudentReservationController.kt
+++ backend/src/main/kotlin/pwr/diplomaproject/controller/StudentReservationController.kt
@@ -1,23 +1,22 @@
 package pwr.diplomaproject.controller
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import pwr.diplomaproject.model.dto.StudentReservationDto
 
-@RestController("/student/reservation")
+@RestController
+@RequestMapping("/student/reservation")
 class StudentReservationController {
 
     @GetMapping
     fun getReservations(): List<StudentReservationDto> = TODO()
 
-    @GetMapping
-    fun getReservation(@RequestParam id: Long): StudentReservationDto = TODO()
+    @GetMapping("/{id}")
+    fun getReservation(@PathVariable id: Long): StudentReservationDto = TODO()
 
-    @GetMapping("/approve")
-    fun approveReservation(@RequestParam id: Long): Unit = TODO()
+    @GetMapping("/approve/{id}")
+    fun approveReservation(@PathVariable id: Long): Unit = TODO()
 
-    @GetMapping("/cancel")
-    fun cancelReservation(@RequestParam id: Long): Unit = TODO()
+    @GetMapping("/cancel/{id}")
+    fun cancelReservation(@PathVariable id: Long): Unit = TODO()
 }
--- backend/src/main/kotlin/pwr/diplomaproject/controller/StudentSubjectController.kt
+++ backend/src/main/kotlin/pwr/diplomaproject/controller/StudentSubjectController.kt
@@ -1,12 +1,11 @@
 package pwr.diplomaproject.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import pwr.diplomaproject.model.dto.StudentSubjectDetailsDto
 import pwr.diplomaproject.model.dto.StudentSubjectDto
 
-@RestController("/student/subject")
+@RestController
+@RequestMapping("/student/subject")
 class StudentSubjectController {
 
     @GetMapping("/available")
@@ -15,6 +14,6 @@ class StudentSubjectController {
     @GetMapping("/proposed")
     fun getProposedSubjects(): List<StudentSubjectDto> = TODO()
 
-    @GetMapping()
-    fun getSubject(@RequestParam id: Long): StudentSubjectDetailsDto = TODO()
+    @GetMapping("/{id}")
+    fun getSubject(@PathVariable id: Long): StudentSubjectDetailsDto = TODO()
 }
