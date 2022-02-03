794c1ddfe907564d9405b0ff94a12a1482f6438e
lukasz <lukasz@tomana.net>
2022-02-03 15:10:45 +0100

    seeder of initial testing data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- Table: app_user
+insert into app_user (id, first_name, last_name, password)
+values (1, 'Jacek', 'Werek', 'password');
+insert into app_user (id, first_name, last_name, password)
+values (2, 'Krzysztof', 'Szarfa', 'password');
+insert into app_user (id, first_name, last_name, password)
+values (3, 'Łukasz', 'Tomcio', 'password');
